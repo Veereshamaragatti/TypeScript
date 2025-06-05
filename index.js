280dea48ee2709cc0cc1208c55f5f4b150df2f28
Veereshamaragatti <veeresh.b.a26@gmail.com>
2025-06-05 15:21:03 +0530

    Basic

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-console.log("Hello, TypeScript!");
-var x = 10;
+"use strict";
+// 2. Create a student object using that type
+const student1 = {
+    name: "Vinod",
+    age: 29,
+    greet: (country) => {
+        return `Welcome! My name is ${student1.name}, I am ${student1.age} yrs old & I am from ${country}`;
+    },
+};
+// 3. A function that introduces a student
+const introduction = (student) => {
+    const { name, age } = student;
+    console.log("Logging details...");
+    console.log(`Name: ${name}, Age: ${age}`);
+    return `Welcome! My name is ${name}, I am ${age} yrs old`;
+};
+// 4. Call the functions
+console.log(introduction(student1));
+console.log(student1.greet("India"));
--- index.ts
+++ index.ts
@@ -1,2 +1,28 @@
-console.log("Hello, TypeScript!")
-let x: number = 10;
+// 1. Define a custom type called Student
+type Student = {
+  name: string;
+  age: number;
+  gender?: string; // optional
+  greet: (country: string) => string;
+};
+
+// 2. Create a student object using that type
+const student1: Student = {
+  name: "Vinod",
+  age: 29,
+  greet: (country: string): string => {
+    return `Welcome! My name is ${student1.name}, I am ${student1.age} yrs old & I am from ${country}`;
+  },
+};
+
+// 3. A function that introduces a student
+const introduction = (student: Student): string => {
+  const { name, age } = student;
+  console.log("Logging details...");
+  console.log(`Name: ${name}, Age: ${age}`);
+  return `Welcome! My name is ${name}, I am ${age} yrs old`;
+};
+
+// 4. Call the functions
+console.log(introduction(student1));
+console.log(student1.greet("India"));
